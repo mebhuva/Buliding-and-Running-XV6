563886723e8d16402c59f12b1f5206de12f9ddec
Mehul Bhuva <mbhuva1@cs.binghamton.edu>
2018-02-10 17:36:33 -0500

    updated main.c file and printing message in kernal mode

--- main.c
+++ main.c
@@ -55,6 +55,8 @@ mpenter(void)
 static void
 mpmain(void)
 {
+if(!cpu->id)
+  cprintf("CS550 proj0 printing in kernel space\n");
   cprintf("cpu%d: starting\n", cpu->id);
   idtinit();       // load idt register
   xchg(&cpu->started, 1); // tell startothers() we're up
